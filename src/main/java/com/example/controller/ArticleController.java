ea599576efdbac78e2354762bd82f380fec110b2
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-09 20:56:45 +0900

    微修正 不要なDI（applicationスコープ）を削除しました。

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -2,8 +2,6 @@ package com.example.controller;
 
 import java.util.List;
 
-import javax.servlet.ServletContext;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
