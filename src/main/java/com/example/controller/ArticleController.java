9ac54bced0924b6c10276f59a47e04f9bde8307e
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-09 15:18:09 +0900

    表示用画面bbs.htmlを作成、ArticleController.javaを編集し、記事一覧の表示の動作確認を行いました。

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -2,9 +2,10 @@ package com.example.controller;
 
 import java.util.List;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
@@ -18,11 +19,21 @@ public class ArticleController {
 	@Autowired
 	private ArticleRepository repository;
 	
+	@Autowired
+	private ServletContext application;
+	
 	@RequestMapping("")
-	public String index(Model model) {
+	public String index(){
 		List<Article> articleList = repository.findAll();
-		model.addAttribute("articleList", articleList);
+		application.setAttribute("articleList", articleList);
 		
 		return "bbs";
+	}
+	
+	@RequestMapping("/create-article")
+	public String createArticle() {
+		
+		
+		return "redirect:";
 	}
 }
