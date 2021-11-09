f6da3ac20e0a4935e497f275edc1658f4f49032b
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-09 14:27:41 +0900

    掲示板作成課題の回答を開始しました。ドメインクラスであるArticle.javaとComment.javaを作成しました。

---
+++ src/main/java/com/example/domain/Article.java
@@ -0,0 +1,42 @@
+package com.example.domain;
+
+/**
+ * 記事を表すドメインクラスです。<br>
+ * @author cyjoh
+ *
+ */
+public class Article {
+	private Integer id;
+	private String name;
+	private String content;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", content=" + content + "]";
+	}
+
+}
